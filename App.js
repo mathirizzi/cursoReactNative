e0b6d5cb4b0692c56339b6d432d30d5bb0c39d8e
Mathias Rizzi <mathiasrizzi@gmail.com>
2022-10-30 20:29:55 -0300

    Commit 301022 2029

--- App.js
+++ App.js
@@ -1,9 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <View style={{
+        flexDirection: 'row',
+        justifyContent: 'space-beetween'
+      }}>
+        <TextInput style={{borderBottomColor: 'black', borderWidth: 1}} />
+          <Button title="Agregar" />
+      </View>
+
+      <View></View>
       <Text>Hola Coder! Este es el proyecto de Mathias Rizzi</Text>
       <StatusBar style="auto" />
     </View>
@@ -13,8 +23,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
